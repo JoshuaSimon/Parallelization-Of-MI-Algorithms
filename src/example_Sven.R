0ca67f3291877c06783f0d3ffef4f3d6767418c1
Sven Nekula <sven.nek1@googlemail.com>
2022-01-05 14:19:31 +0100

    fixed issue in my script (Sven)

--- src/example_Sven.R
+++ src/example_Sven.R
@@ -14,18 +14,39 @@ dataGenerator <- function(n = 10000, p_mis = 0.25){
   x_1 <- rnorm(n, 5, 2)
   # x_2 depending on the values of x_1
   x_2 <- 5 - 0.7 * x_1 + rnorm(n, 0, 4)
-  # x_3 depending on the values of x_1 und x_2
+  # x_3 depending on the values of x_1 and x_2
   x_3 <- 5 + 0.5 * x_1 + 0.3 * x_2 + rnorm(n, 0, 2)
   # x_4 depending on values of x_1, x_2, x_3
   x_4 <- 5 - 0.1 * x_1 + 0.5 * x_2 - 0.2 * x_3 + rnorm(n, 0, 5)
+  # x_5
+  x_5 <- rnorm(n, 8, 4)
+  # x_6 depending on the values of x_5
+  x_6 <- 3 + 0.25 * x_5 + rnorm(n, 2, 3)
+  # x_7 depending on the values of x_5 and x_6
+  x_7 <- 1.3 - 0.4 * x_5 + 0.2 * x_6 + rnorm(n, 1, 8)
+  # x_8
+  x_8 <- rnorm(n, 5, 10)
+  # x_9
+  x_9 <- 7 - 0.1 * x_8 + rnorm(n, 3, 5)
+  # x_10 
+  x_10 <- 11 - 0.5 * x_8 - 0.25 * x_9 + rnorm(n, 2, 4)
   
-  dat <- data.frame(x_1, x_2, x_3)
+  dat <- data.frame(x_1, x_2, x_3, x_4, x_5, x_6, x_7,
+                    x_8, x_9, x_10)
   
   # generating NAs
-  mis <- sample(1:n, p_mis * n, replace = FALSE)
-  mis2 <- sample(1:n, 0.5 * n, replace = FALSE)
-  dat[mis, 3] <- NA
-  dat[mis2, 4] <- NA
+  mis_x3 <- sample(1:n, p_mis * n, replace = FALSE)
+  mis_x4 <- sample(1:n, 0.4 * n, replace = FALSE)
+  mis_x7 <- sample(1:n, 0.2 * n, replace = FALSE)
+  mis_x9 <- sample(1:n, 0.15 * n, replace = FALSE)
+  mis_x10 <- sample(1:n, 0.5 * n, replace = FALSE)
+  
+  
+  dat[mis_x3, 3] <- NA
+  dat[mis_x4, 4] <- NA
+  dat[mis_x7, 7] <- NA
+  dat[mis_x9, 9] <- NA
+  dat[mis_x10, 10] <- NA
   
   return(dat)
 }
@@ -51,10 +72,13 @@ impTimes <- function(x, chains = c(1, seq(10,100,10))){
                 printFlag = FALSE, seed = seed)
     endTime <- Sys.time()
     seqTimes <- c(seqTimes, 
-                  as.numeric(endTime - startTime))
+                  as.numeric(difftime(endTime, startTime, 
+                                      units="secs")))
     print(paste0("Time for ", i, 
                  " imputations (sequential): ",
-                 round(as.numeric(endTime - startTime), 4),
+                 round(as.numeric(difftime(endTime, 
+                                           startTime, 
+                                           units="secs")),4),
                  " seconds"))
     
     # mice.par run
@@ -63,10 +87,13 @@ impTimes <- function(x, chains = c(1, seq(10,100,10))){
                     printFlag = FALSE, seed = seed)
     endTime <- Sys.time()
     miceparTimes<- c(miceparTimes, 
-                     as.numeric(endTime - startTime))
+                     as.numeric(difftime(endTime, startTime, 
+                                         units="secs")))
     print(paste0("Time for ", i, 
                  " imputations (mice.par): ",
-                 round(as.numeric(endTime - startTime), 4),
+                 round(as.numeric(difftime(endTime, 
+                                           startTime, 
+                                           units="secs")),4),
                  " seconds"))
     
     # foreach run
@@ -83,10 +110,13 @@ impTimes <- function(x, chains = c(1, seq(10,100,10))){
     stopCluster(cluster)
     endTime <- Sys.time()
     parTimes <- c(parTimes, 
-                  as.numeric(endTime - startTime))
+                  as.numeric(difftime(endTime, startTime, 
+                                      units="secs")))
     print(paste0("Time for ", i, 
                  " imputations (foreach): ",
-                 round(as.numeric(endTime - startTime), 4),
+                 round(as.numeric(difftime(endTime, 
+                                           startTime, 
+                                           units="secs")),4),
                  " seconds"))
   }
   
@@ -100,6 +130,7 @@ impTimes <- function(x, chains = c(1, seq(10,100,10))){
 
 
 sizes <- c(10000, seq(25000, 100000, 25000))
+sizes <- 100000
 chains <- c(seq(16,96,16))
 nCores <- detectCores()
 
@@ -130,10 +161,13 @@ for (size in sizes){
                     n.imp.core = ifelse(i==1, 1, i%/%nCores))
     endTime <- Sys.time()
     parlmTimes <- c(parlmTimes, 
-                    as.numeric(endTime - startTime))
+                    as.numeric(difftime(endTime, startTime, 
+                                        units="secs")))
     print(paste0("Time for ", i, 
                  " imputations (parlmice): ",
-                 round(as.numeric(endTime - startTime), 4),
+                 round(as.numeric(difftime(endTime, 
+                                           startTime, 
+                                           units="secs")),4),
                  " seconds"))
   }
   
@@ -147,8 +181,6 @@ for (size in sizes){
 }
 
 imputationTimes <- do.call("rbind", dfList)
-#imputationTimes$dataSize <- as.numeric(format(imputationTimes$dataSize,
-#                                   scientific = F))
 
 plotFunction <- function(size=10000){
   imputationTimes %>% filter(dataSize == size) %>% 
@@ -162,7 +194,7 @@ plotFunction <- function(size=10000){
     geom_point(aes(x=imputations, y=parlm, color="parlmice")) +
     geom_line(aes(x=imputations, y=parlm, color="parlmice")) +
     xlab("Number of Imputations") + ylab("Time in seconds") +
-    ggtitle(paste0("Data size: ", size)) + 
+    ggtitle(paste0("Data size: ", format(size, scientific = FALSE))) + 
     scale_x_continuous(breaks=seq(16,96,16))
 }
 
