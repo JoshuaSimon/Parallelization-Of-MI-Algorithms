1d297a0575243c8a649dc87d671a1cc05e949e03
evawolf96 <evawolf@hotmail.de>
2022-01-08 12:09:08 +0100

    example updated, explanation for system.time

--- src/example_eva.R
+++ src/example_eva.R
@@ -4,49 +4,51 @@ library("pacman") # packagemanager
 p_load("mice", "car", "tidyverse", "foreach", "parallel", "doParallel",
        "iterators", "testit", "devtools", "usethis", "micemd")
 
+
 #Data: ESS data 2004 on satisfaction in life 
-source("src/dataGenerator.R")
+source("C:\\Users\\evawo\\Documents\\OFU Survey Statistik\\WS 21 22\\Unvollständige Daten\\Projekt\\Parallelization-Of-MI-Algorithms\\src\\dataGenerator.R")
+
+dat <- dataGenerator()
 
 R <- c(1, 5, 10, 20, 30, 40, 60, 80, 100) #Multiple Imputations 
 n <- NROW(dat)
-
-# create missing values:
-mcar1 <- sample(nrow(dat), round(0.3 * n))
-mcar2 <- sample(nrow(dat), round(0.3 * n))
-
-mar <- order(dat$tvtot + rnorm(n, 0, 0.5 * sd(dat$tvtot)),
-             decreasing = TRUE)[1:(round(0.3 * n))]
-
-is.na(dat$happy[mcar1]) <- TRUE
-is.na(dat$netuse[mcar2]) <- TRUE
-is.na(dat$vote[mar]) <- TRUE
-
+seed <- 42
 
 ### Multiple imputation and Analysis ###
 
 # use all variables as predictors, but just consider linear relationships..
 # initialization:
 ini <- mice(data=dat, m = 1, maxit = 0)
-pred <- ini$pred
+predictorMatrix <- ini$pred
 
 
 ##Parallelization
 
 #Core Idenitification and Clustering
-num_cores <- 7
-cl <- makeCluster(num_cores)
-clusterSetRNGStream(cl, 9956)
-registerDoParallel(cl)
+foreachfunction <- function(data, seed, num_imp, maxit, m, predictorMatrix){
+  num_cores <- detectCores()-1
+  cl <- makeCluster(num_cores)
+  clusterSetRNGStream(cl, seed)
+  registerDoParallel(cl)
+  
+  par_mice <- foreach(i=1:num_imp, .combine = ibind, .packages="mice") %dopar% {
+    mice(data = data, m = m, maxit = maxit, printFlag = FALSE, predictorMatrix = predictorMatrix)
+  }
+  return(par_mice)
+  stopCluster(cl)
+}
 
 #Store imputation results and runtime
 result <- resultp <- resultparl <- resultpar <-   list()
 systp <- syst <- systparl <- systpar <- matrix(nrow = length(R), ncol=3)
-R<- 1
+
 #Sequential and different parallelized MIs
 for(i in 1:length(R)){
-systp[i,] <- system.time(resultp[[i]] <- foreach(no=1:R[i], .combine=ibind, .export = c("dat", "i"), .packages="mice") %dopar% {
- mice(data=dat,m = 1, predictorMatrix = pred, print = F)
- }) [1:3] #run time of parallelized MI
+  
+systp[i,] <- system.time(
+resultp[[i]] <- foreachfunction(data=dat, seed=seed, num_imp = i, maxit=5, m=1, predictorMatrix = predictorMatrix)
+)[1:3]
+#run time of parallelized MI
 
 syst[i,] <- system.time(result[[i]] <- mice(data=dat,m = R[i], predictorMatrix = pred, print = F))[1:3]
 #run time of sequential MI
