f96d4ef177287622367c6625759d4c58dc115d53
JoshuaSimon <joshsimon96@googlemail.com>
2022-01-08 16:06:00 +0100

    Extended benmark  for different parallel backends

--- src/benchmark_core.R
+++ src/benchmark_core.R
@@ -18,7 +18,6 @@ library(mice)
 library(micemd)
 library(parallel)
 library(doParallel)
-#library(doMC)
 
 # Load up the data generator function.
 source("src/dataGenerator.R")
@@ -30,7 +29,7 @@ power_of_two <- function(n) {
 
 # Estimate the parallelism of an algorithm.
 # Following Amdahl's law, one can estimapte the percentage
-# P of an algorithm with can be parallizied by only providing
+# P of an algorithm which can be parallizied by only providing
 # the speed up from one parallel run and the number of
 # processes n.
 estimate_parallelism <- function(speed_up, n) {
@@ -41,9 +40,25 @@ estimate_parallelism <- function(speed_up, n) {
 # want to pass differently named arguments to parlmice form
 # another scope. The reason for this is the internal handling
 # of parlmice with argument lists, which are passed to the cluster.
-parlmice_wrap <- function(data, m, cluster.seed, n.core, n.imp.core) {
+parlmice_wrap <- function(data, m, cluster.seed, n.core, n.imp.core, cl.type) {
     result <- parlmice(data = data, m = m, cluster.seed = cluster.seed,
-                    n.core = n.core, n.imp.core = n.imp.core, maxit = 5)
+                    n.core = n.core, n.imp.core = n.imp.core, cl.type = cl.type,
+                    maxit = 5)
+    return(result)
+}
+
+# Wrapper function for a parallel call of mice using the foreach
+# parallel loop.
+foreach_wrap <- function(data, num_imp, seed, num_cores, backend) {
+    cl <- makeCluster(num_cores, type = backend)
+    clusterSetRNGStream(cl, seed)
+    registerDoParallel(cl)
+
+    result <- foreach(i = 1:num_imp, .combine = ibind, .packages = "mice") %dopar% {
+        mice(data = data, m = 1, maxit = 5, printFlag = FALSE)
+    }
+
+    stopCluster(cl)
     return(result)
 }
 
@@ -58,15 +73,19 @@ assert_output <- function(mids_object, num_imp) {
 
 # Runs different parallel MI algorithms and measures the run time.
 # The run time is averaged over several benchmark runs.
-benmark_imputation <- function(data, num_imp, cores, runs = 5) {
+benmark_imputation <- function(data, num_imp, cores, runs = 5, os_test = FALSE) {
     serial_time <- numeric(length(cores))
     foreach_time <- numeric(length(cores))
     parlmice_time <- numeric(length(cores))
+    foreach_time_fork <- numeric(length(cores))
+    parlmice_time_fork <- numeric(length(cores))
     micemd_time <- numeric(length(cores))
 
     run_serial_time <- numeric(length(runs))
     run_foreach_time <- numeric(length(runs))
     run_parlmice_time <- numeric(length(runs))
+    run_foreach_time_fork <- numeric(length(runs))
+    run_parlmice_time_fork <- numeric(length(runs))
     run_micemd_time <- numeric(length(runs))
 
     print(paste0("Starting ", runs, " benchmark runs for M = ", num_imp, " multiple imputations."))
@@ -94,23 +113,45 @@ benmark_imputation <- function(data, num_imp, cores, runs = 5) {
         print(paste0("Running M = ", num_imp, " multiple imputations on ", num_cores, " core(s)."))
 
         for (run in 1:runs) {
-            # Parallel run with foreach.
-            time_start_par <- Sys.time()
+            # Perform parallel backend benchmark. This is only possible on
+            # Linux or MacOS.
+            if (os_test) {
+                 # Parallel run with foreach.
+                time_start_par <- Sys.time()
+                result_foreach <- foreach_wrap(data = data,
+                                        num_imp = num_imp, seed = seed,
+                                        num_cores = num_cores, backend = "FORK")
+                time_end_par <- Sys.time()
+                time_taken_par <- difftime(time_end_par, time_start_par, units = "secs")
+                run_foreach_time_fork[run] <- as.numeric(time_taken_par)
+                print(paste0("Runtime in parallel execution (foreach [FORK]) is ", time_taken_par, " seconds."))
 
-            cl <- makeCluster(num_cores)
-            clusterSetRNGStream(cl, seed)
-            registerDoParallel(cl)
+                assert_output(result_foreach, num_imp)
 
-            result_foreach <- foreach(i=1:num_imp, .combine = ibind, .packages="mice") %dopar% {
-                mice(data = data, m = 1, maxit = 5, printFlag = FALSE)
+                # Parallel run with built-in mice function.
+                time_start_par_mice <- Sys.time()
+                result_parlmice <- parlmice_wrap(data = data, m = num_imp,
+                                                cluster.seed = seed,
+                                                n.core = num_cores,
+                                                n.imp.core = ceiling(num_imp / num_cores),
+                                                cl.type = "FORK")
+                time_end_par_mice <- Sys.time()
+                time_taken_par_mice <- difftime(time_end_par_mice, time_start_par_mice, units = "secs")
+                run_parlmice_time_fork[run] <- as.numeric(time_taken_par_mice)
+                print(paste0("Runtime in parallel execution (parlmice [FORK]) is ", time_taken_par_mice, " seconds."))
+
+                assert_output(result_parlmice, num_imp)
             }
 
-            stopCluster(cl)
-
+            # Parallel run with foreach.
+            time_start_par <- Sys.time()
+            result_foreach <- foreach_wrap(data = data,
+                                    num_imp = num_imp, seed = seed,
+                                    num_cores = num_cores, backend = "PSOCK")
             time_end_par <- Sys.time()
             time_taken_par <- difftime(time_end_par, time_start_par, units = "secs")
             run_foreach_time[run] <- as.numeric(time_taken_par)
-            print(paste0("Runtime in parallel execution (foreach) is ", time_taken_par, " seconds."))
+            print(paste0("Runtime in parallel execution (foreach [PSOCK]) is ", time_taken_par, " seconds."))
 
             assert_output(result_foreach, num_imp)
 
@@ -119,11 +160,12 @@ benmark_imputation <- function(data, num_imp, cores, runs = 5) {
             result_parlmice <- parlmice_wrap(data = data, m = num_imp,
                                             cluster.seed = seed,
                                             n.core = num_cores,
-                                            n.imp.core = ceiling(num_imp / num_cores))
+                                            n.imp.core = ceiling(num_imp / num_cores),
+                                            cl.type = "PSOCK")
             time_end_par_mice <- Sys.time()
             time_taken_par_mice <- difftime(time_end_par_mice, time_start_par_mice, units = "secs")
             run_parlmice_time[run] <- as.numeric(time_taken_par_mice)
-            print(paste0("Runtime in parallel execution (parlmice) is ", time_taken_par_mice, " seconds."))
+            print(paste0("Runtime in parallel execution (parlmice [PSOCK]) is ", time_taken_par_mice, " seconds."))
 
             assert_output(result_parlmice, num_imp)
 
@@ -143,27 +185,47 @@ benmark_imputation <- function(data, num_imp, cores, runs = 5) {
         foreach_time[i] <- mean(run_foreach_time)
         parlmice_time[i] <- mean(run_parlmice_time)
         micemd_time[i] <- mean(run_micemd_time)
+
+        if (os_test) {
+            foreach_time_fork[i] <- mean(run_foreach_time_fork)
+            parlmice_time_fork[i] <- mean(run_parlmice_time_fork)
+        }
     }
 
     # Assemble data frame with average results and calculate speed ups
     # as well as the estimated parallelism.
     runtime_data <- data.frame(
         cores, serial_time,
-        foreach_time, parlmice_time, micemd_time,
+        foreach_time, foreach_time_fork,
+        parlmice_time, parlmice_time_fork,
+        micemd_time,
         serial_time / foreach_time,
         serial_time / parlmice_time,
+        serial_time / foreach_time_fork,
+        serial_time / parlmice_time_fork,
         serial_time / micemd_time)
 
     colnames(runtime_data) <- c(
         "cores", "serial_time",
-        "foreach_time", "parlmice_time", "micemd_time",
-        "foreach_speed_up", "parlmice_speed_up", "micemd_speed_up")
+        "foreach_time", "foreach_time_fork",
+        "parlmice_time", "parlmice_time_fork",
+        "micemd_time",
+        "foreach_speed_up", "foreach_fork_speed_up",
+        "parlmice_speed_up", "parlmice_fork_speed_up",
+        "micemd_speed_up"
+    )
 
     runtime_data$est_par_foreach <- estimate_parallelism(
         runtime_data$foreach_speed_up,
         runtime_data$cores)
+    runtime_data$est_par_foreach_fork <- estimate_parallelism(
+        runtime_data$foreach_fork_speed_up,
+        runtime_data$cores)
     runtime_data$est_par_parlmice <- estimate_parallelism(
         runtime_data$parlmice_speed_up,
+        runtime_data$cores)
+    runtime_data$est_par_parlmice_fork <- estimate_parallelism(
+        runtime_data$parlmice_fork_speed_up,
         runtime_data$cores)
     runtime_data$est_par_micemd <- estimate_parallelism(
         runtime_data$micemd_speed_up,
@@ -172,7 +234,7 @@ benmark_imputation <- function(data, num_imp, cores, runs = 5) {
     return(runtime_data)
 }
 
-benmark_plot <- function(runtime_data, cores, value = "time") {
+benmark_plot <- function(runtime_data, cores, value = "time", os_test = FALSE) {
     if (value == "time") {
         plot <- ggplot(data = runtime_data) +
             geom_point(aes(cores, foreach_time, color = "foreach")) +
@@ -183,6 +245,14 @@ benmark_plot <- function(runtime_data, cores, value = "time") {
             geom_line(aes(cores, parlmice_time, color = "parlmice")) +
             geom_line(aes(cores, micemd_time, color = "mice.par")) +
             geom_line(aes(cores, serial_time, color = "serial"))
+
+        if (os_test) {
+            plot <- plot +
+                geom_point(aes(cores, foreach_time_fork, color = "foreach_fork")) +
+                geom_point(aes(cores, parlmice_time_fork, color = "parlmice_fork")) +
+                geom_line(aes(cores, foreach_time_fork, color = "foreach_fork")) +
+                geom_line(aes(cores, parlmice_time_fork, color = "parlmice_fork"))
+        }
 
         plot <- plot +
             ggtitle("Runtime of Multiple Imputations") +
@@ -198,6 +268,14 @@ benmark_plot <- function(runtime_data, cores, value = "time") {
             geom_line(aes(cores, foreach_speed_up, color = "foreach")) +
             geom_line(aes(cores, parlmice_speed_up, color = "parlmice")) +
             geom_line(aes(cores, micemd_speed_up, color = "mice.par"))
+
+        if (os_test) {
+            plot <- plot +
+                geom_point(aes(cores, foreach_fork_speed_up, color = "foreach_fork")) +
+                geom_point(aes(cores, parlmice_fork_speed_up, color = "parlmice_fork")) +
+                geom_line(aes(cores, foreach_fork_speed_up, color = "foreach_fork")) +
+                geom_line(aes(cores, parlmice_fork_speed_up, color = "parlmice_fork"))
+        }
 
         plot <- plot +
             ggtitle("Speed Up of Multiple Imputations Running in Parallel") +
@@ -215,11 +293,29 @@ num_cores <- detectCores()
 cores <- c(1, power_of_two(log(num_cores) / log(2)))
 
 data <- dataGenerator(n = 10000)
-runtime_data <- benmark_imputation(
-    data = data, num_imp = 128,
-    cores = cores, runs = 5)
+
+# Check for the OS type of the machine and choose
+# a suitiable benchmark run. On Linux and MacOS
+# different parallel backends are compared.
+os_name <- Sys.info()[["sysname"]]
+if (os_name == "Windows") {
+    runtime_data <- benmark_imputation(
+        data = data, num_imp = 128,
+        cores = cores, runs = 5,
+        os_test = FALSE)
+} else if (os_name %in% c("Darwin", "Linux")) {
+    runtime_data <- benmark_imputation(
+        data = data, num_imp = 128,
+        cores = cores, runs = 5,
+        os_test = TRUE)
+} else {
+    stop("Your OS is not supported by this benchmark.")
+}
 
 # Plot the results.
+benmark_plot(runtime_data, cores, "time", os_test = TRUE)
+benmark_plot(runtime_data, cores, "speed_up", os_test = TRUE)
+
 ggarrange(
     benmark_plot(runtime_data, cores, "time"),
     benmark_plot(runtime_data, cores, "speed_up"),
