f414c7a97074fa04fd062534bb48218cf32dad54
JoshuaSimon <joshsimon96@googlemail.com>
2022-01-12 20:30:24 +0100

    Updated code for core benchmark, utils, functions

--- src/parallel_functions.R
+++ src/parallel_functions.R
@@ -17,7 +17,7 @@ library(furrr)
 # of parlmice with argument lists, which are passed to the cluster.
 # See for the issue tracking:
 # https://github.com/amices/mice/issues/189
-parlmice_wrap <- function(data, m, cluster.seed, n.core, n.imp.core, cl.type) {
+parlmice_wrap <- function(data, m, cluster.seed, n.core, cl.type, n.imp.core) {
     result <- parlmice(data = data, m = m, cluster.seed = cluster.seed,
                     n.core = n.core, n.imp.core = n.imp.core, cl.type = cl.type,
                     maxit = 5)
@@ -41,7 +41,25 @@ foreach_wrap <- function(data, num_imp, seed, num_cores, backend) {
 }
 
 
-# Wrapper function a parallel call of mice using the parLapply
+# Alternative implementation for a parallel mice call using the foreach
+# parallel loop. Here the loop size is matched to the number of CPU cores.
+# The number of imputations is then equally distributed to the threds.
+foreach_wrap_alt <- function(data, num_imp, seed, num_cores, backend) {
+    cl <- makeCluster(num_cores, type = backend)
+    clusterSetRNGStream(cl, seed)
+    registerDoParallel(cl)
+
+    result <- foreach(i = 1:num_cores, .combine = ibind, .packages = "mice") %dopar% {
+        mice(data = data, m = ceiling(num_imp / num_cores),
+            maxit = 5, printFlag = FALSE)
+    }
+
+    stopCluster(cl)
+    return(result)
+}
+
+
+# Wrapper function for a parallel call of mice using the parLapply
 # function.
 parLapply_wrap <- function(data, num_imp, seed, num_cores, backend) {
     cl <- makeCluster(num_cores, type = backend)
@@ -70,9 +88,9 @@ parLapply_wrap <- function(data, num_imp, seed, num_cores, backend) {
 }
 
 
-# Wrapper function a parallel call of mice using the furrr
+# Wrapper function for a parallel call of mice using the furrr
 # function.
-furrr_wrap <- function(data, num_imp, seed, num_cores) {
+furrr_wrap <- function(data, num_imp, seed, num_cores, backend) {
     plan(multisession, workers = num_cores)
     imps <- future_map(rep(1, num_imp), ~mice(data = data, m = 1,
                                         maxit = 5,
@@ -87,4 +105,21 @@ furrr_wrap <- function(data, num_imp, seed, num_cores) {
     } else if (length(imps) == 1) {
        return(imp)
     }
+}
+
+
+# Wrapper function for a parallel call of mice using the par.mice
+# function.
+micemd_wrap <- function(data, num_imp, seed, num_cores, backend) {
+    imps <- mice.par(don.na = data, m = num_imp,
+                    maxit = 5, seed = seed, nnodes = num_cores)
+    return(imps)
+}
+
+
+# Wrapper function for a standard call of mice using.
+mice_wrap <- function(data, num_imp, seed, num_cores, backend) {
+    imps <- mice(data, m = num_imp, maxit = 5,
+                printFlag = FALSE, seed = seed)
+    return(imps)
 }
