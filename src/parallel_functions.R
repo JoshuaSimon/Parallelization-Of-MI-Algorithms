8551c76e3def885e40052d69a1f2138b4a979aba
JoshuaSimon <joshsimon96@googlemail.com>
2022-01-15 00:39:03 +0100

    Updated wrapper and utils for new data structure

--- src/parallel_functions.R
+++ src/parallel_functions.R
@@ -29,7 +29,7 @@ parlmice_wrap <- function(data, m, cluster.seed, n.core, cl.type, n.imp.core) {
 # Implementation for a parallel mice call using the foreach
 # parallel loop. Here, the loop size is matched to the number of CPU cores.
 # The number of imputations is then equally distributed to the threds.
-foreach_wrap <- function(data, num_imp, seed, num_cores, backend) {
+foreach_wrap <- function(data, num_imp, seed, num_cores, backend, n.imp.core = NULL) {
     cl <- makeCluster(num_cores, type = backend)
     clusterSetRNGStream(cl, seed)
     registerDoParallel(cl)
@@ -46,7 +46,7 @@ foreach_wrap <- function(data, num_imp, seed, num_cores, backend) {
 
 # Wrapper function for a parallel call of mice using the parLapply
 # function.
-parLapply_wrap <- function(data, num_imp, seed, num_cores, backend) {
+parLapply_wrap <- function(data, num_imp, seed, num_cores, backend, n.imp.core = NULL) {
     cl <- makeCluster(num_cores, type = backend)
     clusterSetRNGStream(cl, seed)
     clusterExport(cl = cl,
@@ -76,7 +76,7 @@ parLapply_wrap <- function(data, num_imp, seed, num_cores, backend) {
 # Wrapper function for a parallel call of mice using the furrr
 # function. Using num_cores instead of num_imp in rep() and
 # m = num_imp / num_cores speeds up the function significantly.
-furrr_wrap <- function(data, num_imp, seed, num_cores, backend) {
+furrr_wrap <- function(data, num_imp, seed, num_cores, backend, n.imp.core = NULL) {
     plan(multisession, workers = num_cores)
     imps <- future_map(rep(1, num_cores), ~mice(data = data, m = num_imp / num_cores,
                                                 maxit = 5,
@@ -96,7 +96,7 @@ furrr_wrap <- function(data, num_imp, seed, num_cores, backend) {
 
 # Wrapper function for a parallel call of mice using the par.mice
 # function.
-micemd_wrap <- function(data, num_imp, seed, num_cores, backend) {
+micemd_wrap <- function(data, num_imp, seed, num_cores, backend, n.imp.core = NULL) {
     imps <- mice.par(don.na = data, m = num_imp,
                     maxit = 5, seed = seed, nnodes = num_cores)
     return(imps)
@@ -104,7 +104,7 @@ micemd_wrap <- function(data, num_imp, seed, num_cores, backend) {
 
 
 # Wrapper function for a standard call of mice using.
-mice_wrap <- function(data, num_imp, seed, num_cores, backend) {
+mice_wrap <- function(data, num_imp, seed, num_cores, backend, n.imp.core = NULL) {
     imps <- mice(data, m = num_imp, maxit = 5,
                 printFlag = FALSE, seed = seed)
     return(imps)
--- src/utils.R
+++ src/utils.R
@@ -37,58 +37,88 @@ assert_output <- function(mids_object, num_imp) {
 # Timer function to measure the runtime of mice calls from
 # different mice wrapper functions. This function also
 # assserts the output produced by the mice calls. The runtime
-# in seconds is returned as a single value.
-mice_timer <- function(fun, fun_name, data, num_imp, seed, num_cores, backend, exe_type, print_flag = TRUE, ...) {
-    time_start <- Sys.time()
-    imp_result <- fun(data, num_imp, seed, num_cores, backend, ...)
-    time_end <- Sys.time()
-    time_taken <- difftime(time_end, time_start, units = "secs")
+# in seconds is returned as a single value (method = "simple")
+# or as a proc_time object containing user, system and elapsed
+# time (method = "verbose").
+mice_timer <- function(fun, fun_name, method = "simple", print_flag = TRUE,
+    data, num_imp, seed, num_cores, backend, exe_type, ...) {
+
+    if (method == "simple") {
+        time_start <- Sys.time()
+        imp_result <- fun(data, num_imp, seed, num_cores, backend, ...)
+        time_end <- Sys.time()
+        time_taken <- as.numeric(difftime(time_end, time_start, units = "secs"))
+        time_print <- time_taken
+    } else if (method == "verbose") {
+        time_taken <- system.time(
+           imp_result <- fun(data, num_imp, seed, num_cores, backend, ...)
+        )
+        time_print <- time_taken[3]
+    }
 
     if (print_flag) {
         print(paste0(
             "Runtime in ", exe_type, " execution (", fun_name,
-            " [", backend, "]) is ", time_taken, " seconds.")
+            " [", backend, "]) is ", time_print, " seconds.")
         )
     }
 
     assert_output(imp_result, num_imp)
 
-    return(as.numeric(time_taken))
+    return(time_taken)
 }
 
-#Colour Manual for plots
-colourvec <- c("cyan2", "blue3", "darkorchid3", "deeppink2",
-               "sienna2", "red2", "yellow1", "green1", "springgreen4")
-colourtable <- cbind(colourvec, c("foreach", "parlmice", "parLapply", "mice.par",
-                                  "furrr", "serial", "foreach_fork", "parlmice_fork", 
-                                  "parLapply_fork"))
-#for runtime os_test=FALSE
-scmr1 <- scale_colour_manual("", 
-                    breaks = c("foreach", "parlmice", "parLapply", "mice.par", "furrr", "serial"),
-                    values = c("foreach"=colourvec[1], "parlmice"=colourvec[2], 
-                               "parLapply"=colourvec[3], "mice.par"=colourvec[4],
-                               "furrr"=colourvec[5], "serial"=colourvec[6]))
-#for runtime os_test=TRUE
-scmr2 <- scale_colour_manual("", 
-                    breaks = c("foreach", "parlmice", "parLapply", "mice.par", "furrr",         
-                               "serial", "foreach_fork", "parlmice_fork", "parLapply_fork"),
-                    values = c("foreach"=colourvec[1], "parlmice"=colourvec[2], 
-                               "parLapply"=colourvec[3], "mice.par"=colourvec[4],
-                               "furrr"=colourvec[5], "serial"=colourvec[6],
-                               "foreach_fork"=colourvec[7], "parlmice_fork"=colourvec[8],
-                               "parLapply_fork"=colourvec[9]))
-#for speedup os_test=FALSE
-scms1 <- scale_colour_manual("", 
-                    breaks = c("foreach", "parlmice", "parLapply", "mice.par", "furrr"),
-                    values = c("foreach"=colourvec[1], "parlmice"=colourvec[2], 
-                               "parLapply"=colourvec[3], "mice.par"=colourvec[4],
-                               "furrr"=colourvec[5]))
-#for speedup os_test=TRUE
-scms2 <- scale_colour_manual("", 
-                    breaks = c("foreach", "parlmice", "parLapply", "mice.par", "furrr"
-                               , "foreach_fork", "parlmice_fork", "parLapply_fork"),
-                    values = c("foreach"=colourvec[1], "parlmice"=colourvec[2], 
-                               "parLapply"=colourvec[3], "mice.par"=colourvec[4],
-                               "furrr"=colourvec[5],
-                               "foreach_fork"=colourvec[7], "parlmice_fork"=colourvec[8],
-                               "parLapply_fork"=colourvec[9]))
+
+# Returns a custom color map for runtime and speed up plots.
+# The return value can be added to a ggplot object.
+custom_color_map <- function(test_mode = "runtime", os_test = FALSE) {
+    # Define vector containing the custom color map.
+    colourvec <- c("cyan2", "blue3", "darkorchid3", "deeppink2",
+                "sienna2", "red2", "yellow1", "green1", "springgreen4")
+    colourtable <- cbind(colourvec,
+                        c("foreach", "parlmice", "parLapply", "mice.par",
+                        "furrr", "serial", "foreach_fork", "parlmice_fork",
+                        "parLapply_fork"))
+
+    # Switch through different test modes with specifications of os_test,
+    # that is, if the parallel backend is evaluated as well.
+    if (test_mode == "runtime" & os_test == FALSE) {
+        map <- scale_colour_manual("",
+            breaks = c("foreach", "parlmice", "parLapply", "mice.par", "furrr", "serial"),
+            values = c("foreach" = colourvec[1], "parlmice" = colourvec[2],
+                        "parLapply" = colourvec[3], "mice.par" = colourvec[4],
+                        "furrr" = colourvec[5], "serial" = colourvec[6])
+        )
+    } else if (test_mode == "runtime" & os_test == TRUE) {
+        map <- scale_colour_manual("",
+            breaks = c("foreach", "parlmice", "parLapply", "mice.par", "furrr",
+                        "serial", "foreach_fork", "parlmice_fork", "parLapply_fork"),
+            values = c("foreach" = colourvec[1], "parlmice" = colourvec[2],
+                        "parLapply" = colourvec[3], "mice.par" = colourvec[4],
+                        "furrr" = colourvec[5], "serial" = colourvec[6],
+                        "foreach_fork" = colourvec[7], "parlmice_fork" = colourvec[8],
+                        "parLapply_fork" = colourvec[9])
+        )
+    } else if (test_mode == "speed_up" & os_test == FALSE) {
+        map <- scale_colour_manual("",
+            breaks = c("foreach", "parlmice", "parLapply", "mice.par", "furrr"),
+            values = c("foreach" = colourvec[1], "parlmice" = colourvec[2],
+                        "parLapply" = colourvec[3], "mice.par" = colourvec[4],
+                        "furrr" = colourvec[5])
+        )
+    } else if (test_mode == "speed_up" & os_test == TRUE) {
+        map <- scale_colour_manual("",
+            breaks = c("foreach", "parlmice", "parLapply", "mice.par", "furrr"
+                        , "foreach_fork", "parlmice_fork", "parLapply_fork"),
+            values = c("foreach" = colourvec[1], "parlmice" = colourvec[2],
+                        "parLapply" = colourvec[3], "mice.par" = colourvec[4],
+                        "furrr" = colourvec[5],
+                        "foreach_fork" = colourvec[7], "parlmice_fork" = colourvec[8],
+                        "parLapply_fork" = colourvec[9])
+        )
+    } else {
+       stop("Custom color map: This combination of arguments is not valid.")
+    }
+
+    return(map)
+}
